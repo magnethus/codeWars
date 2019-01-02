f9ebfd5c84cfe21d7cd11fa4131e0a909f78570d
Alex Alcala Cuba <Alex Alcala@jalasoft.local>
2019-01-02 17:24:39 -0400

    First

---
+++ codeWars.py
@@ -0,0 +1,12 @@
+import string
+def toJadenCase(string):
+    for i in string:
+         print (i)
+         return;
+
+a = "ow can mirrors be real if our eyes aren't real"
+
+toJadenCase(string.capwords(a))
+
+
+string.capwords("they're bill's friends from the UK")
