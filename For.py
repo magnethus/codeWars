6bbcd9ba48afb4c7a66ec4136c0f2203fd2dd900
alex <magnethusgit config --global user.email magnethus@gmail.comgit config --global user.email magnethus>
2019-01-13 15:26:57 -0800

    forloop

---
+++ For.py
@@ -0,0 +1,21 @@
+for letter in "Giraffe accademy":
+    print(letter)
+
+friends = ["Jim", "Kevin", "Katy"]
+for f in friends:
+    print(f)
+
+for index in range(10):
+    print(index)
+
+for index in range(len(friends)):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+for ind in range(5):
+    if ind == 0:
+        print("First iteration")
+    else:
+        print("Not first")
